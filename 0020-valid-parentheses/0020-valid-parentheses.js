a0cec0ab9a65aa827d32820764861d18906bd05d
pedrojcaceresl <76101142+pedrojcaceresl@users.noreply.github.com>
2022-12-12 22:04:35 -0300

    Time: 118 ms (23.06%), Space: 42.3 MB (77.70%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let keyMap = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{',
+    }
+    
+    for (let i = 0; i <= s.length; i++) {
+
+       if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+           stack.push(s[i]);
+       } else if (stack[stack.length - 1] == keyMap[s[i]]) {
+           stack.pop();
+       } else {
+           return false;
+       }
+        
+    }
+
+    return stack.length ? false : true;
+
+};
