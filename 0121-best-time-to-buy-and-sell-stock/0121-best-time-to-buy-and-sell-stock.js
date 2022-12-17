4d67e624c0703e1b0d56080fa0126c6a93c86519
pedrojcaceresl <76101142+pedrojcaceresl@users.noreply.github.com>
2022-12-17 12:14:57 -0300

    Time: 92 ms (88.96%), Space: 51.8 MB (52.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,13 +6,22 @@ var maxProfit = function(prices) {
     let [left, right, max] = [0, 1, 0];
     
     while (right < prices.length) {
+        
         if (prices[right] <= prices[left]) left = right;
         
         const window = prices[right] - prices[left];
         
-        max = Math.max(max, window);
+        max = Math.max(max, window)
         
         right++;
     }
+    
     return max;
 };
+
+// len 5
+// [7,1,5,3,6,4]
+// left     0 1
+// right    1 2 3 4 5
+// max      0 4 4 5
+// window   0 4 2 5
