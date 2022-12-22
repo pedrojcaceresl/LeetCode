d6df295295a9462b56af3ea96c2f12a696ae149a
pedrojcaceresl <76101142+pedrojcaceresl@users.noreply.github.com>
2022-12-21 21:52:28 -0300

    Time: 85 ms (72.33%), Space: 42.1 MB (90.26%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -4,11 +4,10 @@
  */
 
 var hammingWeight = function(n, sum = 0) {
-    let [bits, mask] = [ 0, 1 ];
+    let [ bits, mask ] = [0, 1];
     
     for (let i = 0; i < 32; i++) {
         const hasBit = ((n & mask) !== 0);
-        
         if (hasBit) bits++;
         
         mask <<= 1;
