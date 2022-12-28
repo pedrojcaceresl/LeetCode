6a9b1715447c69e0c04922f0cef10281a9001996
pedrojcaceresl <76101142+pedrojcaceresl@users.noreply.github.com>
2022-12-27 22:41:36 -0300

    Time: 69 ms (88.63%), Space: 45.1 MB (53.38%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    let left = 0;
+    let right = nums.length - 1;
+    let middle = 0;
+    
+    while (right >= left) {
+        middle = Math.floor((left + right) / 2);
+        
+        if (nums[middle] === target) {
+            return middle;
+        }
+        
+        if (nums[middle] > target) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    
+    return -1;
+};
+
