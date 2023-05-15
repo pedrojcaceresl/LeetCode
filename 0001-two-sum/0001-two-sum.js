c76d7bb23d44fc8253d2079c8a1f01e6d23f5e6e
Pedro Cáceres <76101142+pedrojcaceresl@users.noreply.github.com>
2023-05-14 23:23:08 -0400

    Time: 52 ms (98.31%), Space: 42.4 MB (62.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 const twoSum = (arr, target) => {
-    let obj = {}
+    let obj = {};
     let complement = 0;
     
-    for(let i = 0; i <= arr.length; i++) {
-        complement = target - arr[i]
-        if(obj.hasOwnProperty(complement)) {
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        complement = target - element;
+        if (obj[complement] !== undefined) {
             return [obj[complement], i];
         }
+        
         obj[arr[i]] = i;
     }
+    return -1;
 }
