f6923af20549406d057e7fa536fb6ac23ae975ce
Pedro Cáceres <76101142+pedrojcaceresl@users.noreply.github.com>
2025-03-01 11:52:54 -0300

    Time: 1 ms (92.18%), Space: 43.2 MB (39.59%) - LeetHub

---
+++ 1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int result = 0;
+
+        double  x1 = points[0][0];
+        double  y1 = points[0][1];
+        
+        for (int i = 1; i < points.length; i++) {
+            int x2 = points[i][0];
+            int y2 = points[i][1];
+            // se aplica la distancia de Chebyshev d = max(|x2 - x1|, |y2 - y1|)
+            result += Math.max(Math.abs(y2 - y1), Math.abs(x2 - x1));
+
+            x1 = x2;
+            y1 = y2;
+        }
+
+        return result;
+    }
+}
+
