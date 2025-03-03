28b026364f75e9f19005bd50cb90bd71c091571e
Pedro Cáceres <76101142+pedrojcaceresl@users.noreply.github.com>
2025-03-03 08:26:03 -0300

    Time: 0 ms (100%), Space: 42 MB (8.56%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -14,7 +14,6 @@ class Solution {
         int top = 0, bottom = mLength - 1, left = 0, right = nLength - 1;
 
         while (top <= bottom && left <= right) {
-
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
@@ -26,7 +25,7 @@ class Solution {
             right--;
 
             if (top <= bottom) {
-                for (int i = right; i >= left; i-- ) {
+                for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
                 bottom--;
@@ -36,7 +35,6 @@ class Solution {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
                 }
-
                 left++;
             }
         }
